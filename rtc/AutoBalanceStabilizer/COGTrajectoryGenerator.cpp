8804bc48319bb1bea19f7bc66dd21fda4f74002c
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2019-11-30 15:16:37 +0900

    tmp_run

--- rtc/AutoBalanceStabilizer/COGTrajectoryGenerator.cpp
+++ rtc/AutoBalanceStabilizer/COGTrajectoryGenerator.cpp
@@ -26,4 +26,158 @@ void COGTrajectoryGenerator::calcCogFromZMP(const std::deque<hrp::Vector3>& refz
     }
 }
 
+void COGTrajectoryGenerator::calcCogFromLandingPoints(const hrp::Vector3& support_point,
+                                                      const hrp::Vector3& landing_point,
+                                                      const double dt,
+                                                      const double start_time,
+                                                      const double supporting_time,
+                                                      const double landing_time,
+                                                      const double cur_time)
+{
+    const double g_acc = 9.80665;
+    const double height = 0.05;
+    const double take_off_z_vel = std::sqrt(2 * g_acc * height);
+    const double flight_time = 2 * take_off_z_vel / g_acc;
+
+    const double cog_height = 0.8;
+    const double omega = std::sqrt(g_acc / cog_height);
+    const double rel_cur_time = cur_time - start_time;
+    const double rel_land_time = landing_time - start_time;
+    const double omega_tau = omega * supporting_time;
+
+    // TODO: 本当はx,yのみ
+    const hrp::Vector3 cp = cog + cog_vel / omega;
+    const double omega_T = omega * flight_time;
+    // hrp::Vector3 c_1 = cp - support_point + (landing_point - support_point) / (1 + omega_T) * std::exp(-omega * (supporting_time - rel_cur_time));
+    // c_1 /= -omega * (1 / flight_time + omega / 2) * std::exp((2 * rel_land_time - rel_cur_time) * omega) +
+    //     0.5 * omega * omega * omega * (1 / omega + 2 * rel_cur_time) * std::exp(omega * rel_cur_time) +
+    //     (omega * (1 / flight_time + omega / 2) * (1 - omega_T)) / (1 + omega_T) * std::exp((2 * rel_land_time - 2 * supporting_time + rel_cur_time) * omega) -
+    //     0.5 * omega * omega * omega * (1 / omega + 2 * supporting_time + 3 * flight_time + 2 * omega_T * supporting_time) * std::exp(omega_T);
+
+    // ddot(x) の条件でといた
+    // hrp::Vector3 c_1 = 2 * (cp - support_point - (landing_point - support_point) / (omega_T + 1) * std::exp(-omega * (supporting_time - rel_cur_time)));
+    // c_1 /= omega * omega * (1 + 2 * omega * rel_cur_time) * std::exp(omega * rel_cur_time) +
+    //     -(omega_T + 2) / flight_time * omega * std::exp(omega * (2 * rel_land_time - rel_cur_time)) +
+    //     -omega * omega / (omega_T + 1) * (1 + 2 * omega * supporting_time + 2 * omega * flight_time + 2 * omega * supporting_time * flight_time) * std::exp(omega * rel_cur_time) +
+    //     (omega_T + 2) / (flight_time * (omega_T + 1)) * omega * (1 - omega_T) * std::exp(omega * (2 * rel_land_time - 2 * supporting_time + rel_cur_time));
+
+    // これは CP の1階微分方程式を解いた結果
+    // hrp::Vector3 c_1 = cp - support_point - (landing_point - support_point) / (omega_T + 1) * std::exp(-omega * (supporting_time - rel_cur_time));
+    // c_1 /= ((flight_time + supporting_time + omega * supporting_time * flight_time) / (omega_T + 1) + rel_cur_time) * omega * omega * omega * std::exp(omega * rel_cur_time) +
+    //     (omega + flight_time / 2 * omega * omega - 1 / flight_time - 0.5 * omega) / (omega_T + 1) * omega * std::exp(omega * (2 * rel_land_time - 2 * supporting_time + rel_cur_time)) +
+    //     omega * omega * omega * rel_cur_time * std::exp(omega * rel_cur_time) +
+    //     -(omega / flight_time + 0.5 * omega * omega) * std::exp(omega * (2 * rel_land_time - rel_cur_time));
+
+    // 3区間に分けて考える
+    std::vector<double> tau(3);
+    const double diff_tau = supporting_time / 3.0;
+    tau[0] = diff_tau;
+    tau[1] = tau[0] + diff_tau;
+    tau[2] = supporting_time;
+
+    hrp::Vector3 start_cp = support_point;
+    hrp::Vector3 c_1 = hrp::Vector3::Zero();
+
+    const hrp::Vector3 start_zmp_offset = hrp::Vector3(-0.1, 0, 0);
+    const hrp::Vector3 end_zmp_offset = hrp::Vector3(0.1, 0, 0);
+    // const hrp::Vector3 start_zmp_offset = hrp::Vector3(-0.05, 0, 0);
+    // const hrp::Vector3 end_zmp_offset = hrp::Vector3(0.05, 0, 0);
+
+    std::vector<hrp::Vector3> an(3);
+    std::vector<hrp::Vector3> bn(3);
+
+    // TODO: input_zmpの接続の確認、cog_velの接続も
+
+    // an.back() = (end_zmp_offset - start_zmp_offset) * 2 / 5.0 / diff_tau;
+    an.back() = (end_zmp_offset - start_zmp_offset) * -1 / 20.0 / diff_tau;
+    // an.back() = (end_zmp_offset - start_zmp_offset) / 3.0 / diff_tau;
+    // an.back() = hrp::Vector3::Zero();
+    // bn.back() = support_point;
+    bn.back() = (support_point + end_zmp_offset) - an.back() * supporting_time;
+
+    an[0] = an.back();
+    // bn[0] = support_point;
+    bn[0] = support_point + start_zmp_offset;
+
+    // an[1] = an.back();
+    an[1] = ((end_zmp_offset - start_zmp_offset) - an.back() * (tau[2] - tau[1]) - an[0] * (tau[2] - tau[1])) / (tau[1] - tau[0]);
+    bn[1] = bn[0] + an[0] * tau[0] - an[1] * tau[0];
+
+    // const auto calcC1FromStartCp = [&](const double a, const double b, const hrp::Vector3& start_cp) {
+    //     return (cp - (b + (1 / omega + rel_cur_time) * a + (start_cp - b - (1 / omega + tau) * a) * std::exp(omega * (rel_cur_time - tau)))) /
+    //     (omega * omega * omega * rel_cur_time * std::exp(omega * rel_cur_time) +
+    //      -(omega * omega * flight_time + 2 * omega) / (2 * flight_time) * std::exp(omega * (2 * rel_land_time - rel_cur_time)) +
+    //      -omega * omega * omega * tau * std::exp(omega * rel_cur_time) +
+    //      (omega * omega * flight_time + 2 * omega) / (2 * flight_time) * std::exp(omega * (2 * rel_land_time + rel_cur_time - 2 * tau)));
+    // }
+
+    const auto calcC1OfCurrentPhase = [&](const size_t idx) {
+        // hrp::Vector3 Dn = an.back() * std::exp(-omega * tau[tau.size() - 2]) - an[idx] * std::exp(-omega * tau[idx]);
+        // for (size_t i = idx; i < an.size() - 2; ++i) {
+        //     Dn += an[i + 1] * (std::exp(-omega * tau[i]) - std::exp(-omega * tau[i + 1]));
+        // }
+        // Dn /= omega;
+
+        hrp::Vector3 Dn = hrp::Vector3::Zero();
+        for (size_t i = idx; i < an.size() - 1; ++i) {
+            Dn += (an[idx + 1] - an[idx]) * std::exp(-omega * tau[idx]);
+        }
+        Dn /= omega;
+
+        return (cp - (bn[idx] + (rel_cur_time + 1 / omega) * an[idx] +
+                      Dn * std::exp(omega * rel_cur_time) +
+                      (landing_point - (flight_time + 1 / omega - supporting_time) * an.back() - bn.back()) / (omega_T + 1) * std::exp(-omega * (supporting_time - rel_cur_time)))) /
+        (omega * omega * omega * rel_cur_time * std::exp(omega * rel_cur_time) +
+         -(omega_T + 2) / (2 * flight_time) * omega * std::exp(omega * (2 * rel_land_time - rel_cur_time)) +
+         -(omega * flight_time * supporting_time + supporting_time + flight_time) / (omega_T + 1) * omega * omega * omega * std::exp(omega * rel_cur_time) +
+         -(omega * omega * flight_time * flight_time + omega_T - 2) / (2 * flight_time * (omega_T + 1)) * omega * std::exp(omega * (2 * rel_land_time - 2 * supporting_time + rel_cur_time)));
+    };
+
+    size_t idx = 0;
+    while (rel_cur_time >= tau[idx] && idx < tau.size() - 1) ++idx;
+    c_1 = calcC1OfCurrentPhase(idx);
+
+    hrp::Vector3 ref_zmp = an[idx] * rel_cur_time + bn[idx]; // Debug
+
+    // for (size_t i = 0; i < tau.size(); ++i) {
+    //     if (rel_cur_time <
+    // if (rel_cur_time < diff_tau) {
+    //     c_1 = calcC1OfCurrentPhase(a1, b1, an, bn);
+    //     ref_zmp = a1 * rel_cur_time + b1;
+    // } else if (rel_cur_time < diff_tau * 2) {
+    //     c_1 = calcC1OfCurrentPhase(a2, b2, an, bn);
+    //     ref_zmp = a2 * rel_cur_time + b2;
+    // } else {
+    //     c_1 = calcC1OfCurrentPhase(an, bn, an, bn);
+    //     ref_zmp = an * rel_cur_time + bn;
+    // }
+
+    const hrp::Vector3 lambda = -(std::exp(omega * rel_cur_time) + (omega_T + 2) / (omega_T) * std::exp(omega * (2 * rel_land_time - rel_cur_time))) * c_1;
+
+    // const hrp::Vector3 lambda = -(cp - support_point + (landing_point - support_point) / (1 + omega_T) * std::exp(-omega * (rel_land_time - rel_cur_time))) * (std::exp(omega * rel_cur_time) + (omega_T + 2) / omega_T * std::exp(omega * (2 * rel_land_time - rel_cur_time))) /
+    //     (-omega * (1 / flight_time + omega / 2) * std::exp(omega * (2 * rel_land_time - rel_cur_time)) +
+    //      0.5 * omega * omega * omega * (1 / omega + 2 * rel_cur_time) * std::exp(omega * rel_cur_time) +
+    //      omega * (1 / rel_cur_time + omega / 2) * (1 - omega_T) / (1 + omega_T) * std::exp(omega * (2 * rel_land_time - 2 * supporting_time + rel_cur_time)) -
+    //      0.5 * omega * omega * omega * (1 / omega + 2 * supporting_time + 3 * flight_time + 2 * omega_T * supporting_time) * std::exp(omega * rel_cur_time));
+
+    hrp::Vector3 input_zmp = support_point + omega * omega * lambda;
+
+    // hrp::Vector3 input_zmp = support_point + 2 * (landing_point - support_point) * (std::exp(omega * rel_cur_time) - std::exp(-omega * rel_cur_time)) / ((omega_T + 3) * std::exp(omega * supporting_time) + (3 * omega_T + 1) * std::exp(-omega * supporting_time));
+
+    if (rel_cur_time < supporting_time) cog_acc = omega * omega * (cog - input_zmp); // TODO: X, Y, Zに分解
+    else {
+        cog_acc = hrp::Vector3(0, 0, -g_acc); // Flight phase
+        input_zmp = hrp::Vector3::Zero();
+        ref_zmp = hrp::Vector3::Zero();
+    }
+
+    cog_acc[2] = 0;
+    cog += cog_vel * dt + cog_acc * dt * dt * 0.5;
+    cog_vel += cog_acc * dt;
+    // cog_acc = input_zmp;
+    // cog_acc = ref_zmp;
+    cog_acc[1] = input_zmp[0];
+    cog_acc[2] = ref_zmp[0];
+}
+
 }
--- rtc/AutoBalanceStabilizer/test/testCOGTrajectoryGenerator.cpp
+++ rtc/AutoBalanceStabilizer/test/testCOGTrajectoryGenerator.cpp
@@ -16,25 +16,31 @@ constexpr double dt = 0.002;
 constexpr double max_time = 8.0;
 constexpr size_t max_count = static_cast<size_t>(std::round(max_time / dt));
 const hrp::Vector3 init_cog(0, 0, 0.8);
+
+std::vector<hrp::Vector3> cog_list;
+std::vector<hrp::Vector3> cogvel_list;
+std::vector<hrp::Vector3> cogacc_list;
+std::vector<double> time_list;
 }
 
-void testPreviewController(std::vector<hrp::Vector3>& cog_list, std::vector<double>& time_list)
+void testPreviewController()
 {
     constexpr double preview_time = 1.6;
     constexpr size_t preview_window = static_cast<size_t>(std::round(preview_time / dt));
 
     cog_list.resize(max_count);
+    cogvel_list.resize(max_count);
+    cogacc_list.resize(max_count);
     time_list.resize(max_count);
 
     std::deque<hrp::Vector3> ref_zmp_list;
     ref_zmp_list.emplace_back(0, 0, 0);
 
-
     hrp::COGTrajectoryGenerator cog_traj_gen(init_cog);
     cog_traj_gen.initPreviewController(dt, ref_zmp_list.front());
 
     for (size_t i = 0; i < max_count; ++i) {
-        double cur_time = i * dt;
+        const double cur_time = i * dt;
         time_list.push_back(cur_time);
 
         while (ref_zmp_list.size() < preview_window) {
@@ -57,10 +63,82 @@ void testPreviewController(std::vector<hrp::Vector3>& cog_list, std::vector<doub
 
         cog_traj_gen.calcCogFromZMP(ref_zmp_list);
 
-        cog_list[i] = cog_traj_gen.getCog();
-        time_list[i] = cur_time;
+        cog_list[i]    = cog_traj_gen.getCog();
+        cogvel_list[i] = cog_traj_gen.getCogVel();
+        cogacc_list[i] = cog_traj_gen.getCogAcc();
+        time_list[i]   = cur_time;
 
         ref_zmp_list.pop_front();
+    }
+}
+
+void testFootGuidedRunning()
+{
+    constexpr double _max_time = 4.0;
+    constexpr size_t _max_count = static_cast<size_t>(std::round(_max_time / dt));
+    cog_list.resize(_max_count);
+    cogvel_list.resize(_max_count);
+    cogacc_list.resize(_max_count);
+    time_list.resize(_max_count);
+
+    std::vector<hrp::Vector3> landing_points;
+    std::vector<size_t> landing_counts;
+    std::vector<size_t> supporting_counts;
+
+    const hrp::Vector3 _init_cog = hrp::Vector3(0, 0.1, 0.8); // Start from left kicking
+    hrp::Vector3 one_step(0.3, -0.2, 0.0);
+
+    // 0.5[s] for one step, including supporting time 0.2[s]
+    constexpr double step_time = 1.0;
+    constexpr double support_time = 0.7;
+    constexpr size_t step_count = static_cast<size_t>(std::round(step_time / dt));
+    constexpr size_t support_count = static_cast<size_t>(std::round(support_time / dt));
+
+    landing_points.emplace_back(_init_cog[0], _init_cog[1], 0);
+    landing_counts.push_back(0);
+    supporting_counts.push_back(support_count);
+
+    hrp::COGTrajectoryGenerator cog_traj_gen(_init_cog);
+    for (double count = step_count; count < _max_count * 2; count += step_count) {
+        landing_points.push_back(landing_points.back());
+        landing_points.back() += one_step;
+        landing_counts.push_back(landing_counts.back());
+        landing_counts.back() += step_count;
+        supporting_counts.push_back(support_count);
+        one_step[1] *= -1;
+
+        std::cerr << "landing point: " << landing_points.back().transpose() << std::endl;
+        std::cerr << "landing time:  " << landing_counts.back() * dt << std::endl;
+    }
+
+    size_t cur_idx = 0;
+    for (size_t i = 0; i < _max_count; ++i) {
+        const double cur_time = i * dt;
+
+        if (i == landing_counts[cur_idx + 1]) {
+            ++cur_idx;
+            // _init_cog = cog_list[i - 1];
+            std::cerr << "count: " << i << std::endl;
+            // std::cerr << "init cog: " << _init_cog.transpose() << std::endl;
+            std::cerr << "next landing: " << landing_points[cur_idx].transpose() << std::endl;
+        }
+
+        cog_traj_gen.calcCogFromLandingPoints(landing_points[cur_idx],
+                                              landing_points[cur_idx + 1],
+                                              // _init_cog,
+                                              dt,
+                                              landing_counts[cur_idx] * dt,
+                                              supporting_counts[cur_idx] * dt,
+                                              landing_counts[cur_idx + 1] * dt,
+                                              i * dt);
+
+        constexpr double g_acc = 9.80665;
+        constexpr double omega = std::sqrt(g_acc / 0.8);
+        // cog_list[i] = cog_traj_gen.getCog() + cog_traj_gen.getCogVel() / omega; // CP
+        cog_list[i]    = cog_traj_gen.getCog();
+        cogvel_list[i] = cog_traj_gen.getCogVel();
+        cogacc_list[i] = cog_traj_gen.getCogAcc();
+        time_list[i]   = cur_time;
     }
 }
 
@@ -79,16 +157,18 @@ int main(int argc, char **argv)
         }
     }
 
-    std::vector<hrp::Vector3> cog_list;
-    std::vector<double> time_list;
-
-    testPreviewController(cog_list, time_list);
+    // testPreviewController(cog_list, time_list);
+    testFootGuidedRunning();
 
     const std::string fname("/tmp/testCOGTrajectoryGenerator.dat");
     std::ofstream ofs(fname);
 
     for (size_t i = 0; i < cog_list.size(); ++i) {
-        ofs << time_list[i] << " " << cog_list[i][0] << " " << cog_list[i][1] << " " << cog_list[i][2] << " " << std::endl;
+        ofs << time_list[i] << " "
+            << cog_list[i][0] << " " << cog_list[i][1] << " " << cog_list[i][2] << " "
+            << cogvel_list[i][0] << " " << cogvel_list[i][1] << " " << cogvel_list[i][2] << " "
+            << cogacc_list[i][0] << " " << cogacc_list[i][1] << " " << cogacc_list[i][2] << " "
+            << std::endl;
     }
 
     ofs.close();
@@ -104,7 +184,7 @@ int main(int argc, char **argv)
         fprintf(gp, "plot \"%s\" using 1:2 with lines title \"Cog X\"\n", fname.c_str());
         fprintf(gp, "set title \"COG Y\"\n");
         fprintf(gp, "plot \"%s\" using 1:3 with lines title \"Cog Y\"\n", fname.c_str());
-        // fprintf(gp, "unset multiplot\n");
+        fprintf(gp, "unset multiplot\n");
         fflush(gp);
 
         std::cerr << "Type some character to finish this test: " << std::flush;
